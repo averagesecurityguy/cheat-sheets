67975f544fd9ccb9c7163c7b429da3b49f5f91bf
Stephen Haywood <stephen@averagesecurityguy.info>
2015-08-12 09:11:29 -0400

    Reducing user interaction.

--- kali_light_vbox.sh
+++ kali_light_vbox.sh
@@ -1,23 +1,47 @@
-#!/bin/sh
+#!/bin/bash
+
+#----------------------------------------------------------------------------
+# The Kali light distro is a minimal install that requires some extra steps
+# to install the necessary tools. This script will do the following:
+#
+#   * Install packages needed to build the VirtualBox Guest Additions
+#   * Install the VirtualBox Guest Additions
+#   * Install the Kali top 10 tools and set a random password on the MySQL
+#     database.
+#   * Configure PostgreSQL to start on boot
+#   * Initialize the MSF database
+#   * Reboot the computer.
+#
+# Prior to running this script, insert the VirtualBox Guest Additions CD
+#-----------------------------------------------------------------------------
+
+echo "Updating packages"
+apt-get update
+apt-get -y upgrade
 
 echo "Installing build tools and headers."
-apt-get update
-apt-get upgrade
-apt-get install build-essential linux-headers-$(uname -r)
+apt-get -y install build-essential linux-headers-$(uname -r)
 
 echo "Installing the Virtual Box Guest Additions."
-echo "Insert the VirtualBox Guest Additions CD."
 mount /media/cdrom
-sh /media/cdrom/VBoxLinuxAdditions.run
+if [ ! -f "/media/cdrom/VBoxLinuxAdditions.run" ]; then
+    echo "VirtualBox Guest Additions CD not mounted, skipping."
+else
+    /bin/bash /media/cdrom/VBoxLinuxAdditions.run
+fi
 
 echo "Installing Kali top 10 tools."
 pass=$(head -c 24 /dev/urandom | base64)
 echo "MySQL Root Password: $pass"
-debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password $pass"
-debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password $pass"
-apt-get install kali-linux-top10 seclists
+echo "mysql-server-5.5 mysql-server/root_password_again password $pass" | debconf-set-selections
+echo "mysql-server-5.5 mysql-server/root_password password $pass" | debconf-set-selections
+echo "wireshark-common/install-setuid false" | debconf-set-selections
+apt-get -y install kali-linux-top10 seclists
 
 echo "Configuring Metasploit Database"
 /etc/init.d/postgresql start
 update-rc.d postgresql enable
 msfdb init
+
+echo "Rebooting."
+reboot
