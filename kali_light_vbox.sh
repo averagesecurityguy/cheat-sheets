0d76e75266d3f53792e4287909794c751e134cec
Stephen Haywood <stephen@averagesecurityguy.info>
2015-08-12 13:00:47 -0400

    Apt-get quiet output. Don't reboot automatically. Prompt to ensure Additions CD is loaded.

--- kali_light_vbox.sh
+++ kali_light_vbox.sh
@@ -15,36 +15,35 @@
 #
 # Prior to running this script, insert the VirtualBox Guest Additions CD
 #-----------------------------------------------------------------------------
-
 echo "Updating packages"
 apt-get update
-apt-get -y upgrade
+apt-get -q -y upgrade
 
 echo "Installing build tools and headers."
-apt-get -y install build-essential linux-headers-$(uname -r)
+apt-get -q -y install build-essential linux-headers-$(uname -r)
 
 echo "Installing the VirtualBox Guest Additions."
+read -p "Insert the VirtualBox Guest Additions CD and press [Enter] key..."
 mount /media/cdrom
 if [ ! -f "/media/cdrom/VBoxLinuxAdditions.run" ]; then
     echo "VirtualBox Guest Additions CD not mounted, skipping."
 else
     /bin/bash /media/cdrom/VBoxLinuxAdditions.run
+    umount /media/cdrom
+    eject /media/cdrom
 fi
-umount /media/cdrom
-eject /media/cdrom
 
 echo "Installing Kali top 10 tools."
 pass=$(head -c 24 /dev/urandom | base64)
-echo "MySQL Root Password: $pass"
 echo "mysql-server-5.5 mysql-server/root_password_again password $pass" | debconf-set-selections
 echo "mysql-server-5.5 mysql-server/root_password password $pass" | debconf-set-selections
 echo "wireshark-common/install-setuid false" | debconf-set-selections
-apt-get -y install kali-linux-top10 seclists
+apt-get -q -y install kali-linux-top10 seclists
 
 echo "Configuring Metasploit Database"
 /etc/init.d/postgresql start
 update-rc.d postgresql enable
 msfdb init
 
-echo "Rebooting."
-reboot
+echo "MySQL Root Password: $pass"
+echo "Please reboot."
