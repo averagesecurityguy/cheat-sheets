ff51ba3c7cd62c1bc58eed306ace62128898daac
Stephen Haywood <stephen@averagesecurityguy.info>
2020-07-16 16:43:48 -0400

    Update pre-commit-hooks.sh

--- pre-commit-hooks.sh
+++ pre-commit-hooks.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Checking formatting..."
-if [ "$(gofmt -s -l . | wc -l)" -gt 0 ]; then
+if [ "$(gofmt -w -l . | wc -l)" -gt 0 ]; then
         echo "Please run go fmt"
         exit 1
 fi
